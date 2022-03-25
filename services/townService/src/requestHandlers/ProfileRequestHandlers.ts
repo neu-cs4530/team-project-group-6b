e2201f29e132e7f00b89f31b43ffb6caca0f4671
Jonathan-Shih <jonathan25shih@gmail.com>
2022-03-25 19:01:02 -0400

    Update ProfileRequestHandlers.ts

--- services/townService/src/requestHandlers/ProfileRequestHandlers.ts
+++ services/townService/src/requestHandlers/ProfileRequestHandlers.ts
@@ -1,8 +1,9 @@
+import { WithId, ObjectId } from 'mongodb';
 import getProfileCollection from '../database/getProfileCollection';
 
 /**
- * Envelope that wraps any response from the server
- */
+* Envelope that wraps any response from the server
+*/
 export interface ResponseEnvelope<T> {
   isOK: boolean;
   message?: string;
@@ -15,17 +16,31 @@ export interface CreateProfileRequest {
   email: string
 }
 
-export interface CreateProfileResponse {
-  profileId: string
+export interface Doc {
+  doc: WithId<Document>;
 }
 
-export async function createProfile(requestData: CreateProfileRequest): Promise<ResponseEnvelope<CreateProfileResponse>> {
+export async function createProfile(requestData: CreateProfileRequest): Promise<ResponseEnvelope<string>> {
   try {
     const collection = await getProfileCollection();
     const result = await collection.insertOne(requestData);
     return { isOK: true,
-      response: { profileId: result.insertedId.toHexString() } };
+      response: result.insertedId.toHexString() };
   } catch (err) {
     return { isOK: false, message: 'went to shit, sorry' };
   }
+}
+    
+export async function fetchProfile(id: string): Promise<ResponseEnvelope<any>> {
+  const collection = await getProfileCollection();
+  const result = await collection.findOne<any>(new ObjectId(id));
+
+  if (result !== null && result !== undefined) {
+    return { isOK: true,
+      response: result,
+    }; 
+  }
+  
+  return { isOK: false,
+    message: 'profile not found' };
 }
